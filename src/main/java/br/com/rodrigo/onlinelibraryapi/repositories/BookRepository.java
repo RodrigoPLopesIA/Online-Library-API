5e6c35ed0254b43ec75d8dbe66aedae2262e85f0
Rodrigo Lopes <rodrigo.plopesti@gmail.com>
2025-06-28 16:32:48 -0300

    feat: add query methods for finding books by title, deleting by genre, and updating by author ID in BookRepository

--- src/main/java/br/com/rodrigo/onlinelibraryapi/repositories/BookRepository.java
+++ src/main/java/br/com/rodrigo/onlinelibraryapi/repositories/BookRepository.java
@@ -1,14 +1,33 @@
 package br.com.rodrigo.onlinelibraryapi.repositories;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.rodrigo.onlinelibraryapi.entities.Book;
+import br.com.rodrigo.onlinelibraryapi.entities.Genre;
+import jakarta.transaction.Transactional;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, UUID> {
 
+
+    @Query ("SELECT b FROM Book b WHERE b.title LIKE %:title%")
+    List<Book> findAllBooksLikeTitle(@Param("title") String title);
+
+    @Modifying()
+    @Transactional()
+    @Query("DELETE FROM Book b WHERE b.genre = :genre")
+    Book deleteBookByGenre(Genre genre);
+
+    @Modifying()
+    @Transactional()
+    @Query("UPDATE Book b SET b.title = :book.title, b.publicationDate = :book.publicationDate, b.genre = :book.genre, b.price = :book.price WHERE b.author.id = :author_id")
+    Book updateBookByAuthorId(UUID author_id, Book book);
     
 }
