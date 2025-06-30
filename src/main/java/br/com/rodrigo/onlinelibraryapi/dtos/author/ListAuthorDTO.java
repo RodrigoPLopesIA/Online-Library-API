3630517cf8e6810625703e68140987a29f5fb54e
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-06-30 20:30:09 -0300

    refactor: update ListAuthorDTO to include fields and constructor for Author entity

--- src/main/java/br/com/rodrigo/onlinelibraryapi/dtos/author/ListAuthorDTO.java
+++ src/main/java/br/com/rodrigo/onlinelibraryapi/dtos/author/ListAuthorDTO.java
@@ -1,5 +1,18 @@
 package br.com.rodrigo.onlinelibraryapi.dtos.author;
 
-public record ListAuthorDTO() {
-    
+import java.sql.Date;
+import java.time.Instant;
+import java.util.UUID;
+
+import br.com.rodrigo.onlinelibraryapi.entities.Author;
+
+public record ListAuthorDTO(UUID id, String name, Date dateBirth, String nationality, Instant createdAt,
+        Instant updatedAt) {
+
+    public ListAuthorDTO(Author author) {
+        this(author.getId(), author.getName(), author.getDateBirth(), author.getNationality(), author.getCreatedAt(),
+                author.getUpdatedAt());
+
+    }
+
 }
