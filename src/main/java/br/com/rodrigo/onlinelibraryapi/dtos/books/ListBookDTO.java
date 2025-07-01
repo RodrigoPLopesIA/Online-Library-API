1b28f9a587f7ec4bdddf6f0295ea4c7b0cffd5cc
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-06-30 21:21:05 -0300

    refactor: include ListAuthorDTO in ListBookDTO for improved author representation

--- src/main/java/br/com/rodrigo/onlinelibraryapi/dtos/books/ListBookDTO.java
+++ src/main/java/br/com/rodrigo/onlinelibraryapi/dtos/books/ListBookDTO.java
@@ -5,14 +5,15 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import br.com.rodrigo.onlinelibraryapi.dtos.author.ListAuthorDTO;
 import br.com.rodrigo.onlinelibraryapi.entities.Book;
 import br.com.rodrigo.onlinelibraryapi.enums.Genre;
 
 public record ListBookDTO(UUID id, String title, String isbn, LocalDate publicationDate, Genre genre, BigDecimal price,
-        Instant createdAt, Instant updatedAt) {
+        ListAuthorDTO author, Instant createdAt, Instant updatedAt) {
 
     public ListBookDTO(Book book) {
         this(book.getId(), book.getTitle(), book.getIsbn(), book.getPublicationDate(), book.getGenre(), book.getPrice(),
-                book.getCreatedAt(), book.getUpdatedAt());
+                new ListAuthorDTO(book.getAuthor()), book.getCreatedAt(), book.getUpdatedAt());
     }
 }
