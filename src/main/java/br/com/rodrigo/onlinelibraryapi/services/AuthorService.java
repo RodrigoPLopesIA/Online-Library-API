c558108c7a95b9f47f6c4d862c3c192d69a504aa
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-07-01 19:17:48 -0300

    refactor: move index method to AuthorService for author retrieval with filtering support

--- src/main/java/br/com/rodrigo/onlinelibraryapi/services/AuthorService.java
+++ src/main/java/br/com/rodrigo/onlinelibraryapi/services/AuthorService.java
@@ -1,11 +1,17 @@
 package br.com.rodrigo.onlinelibraryapi.services;
 
+import java.sql.Date;
 import java.util.UUID;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import br.com.rodrigo.onlinelibraryapi.dtos.author.CreateAuthorDTO;
+import br.com.rodrigo.onlinelibraryapi.dtos.author.ListAuthorDTO;
 import br.com.rodrigo.onlinelibraryapi.entities.Author;
 import br.com.rodrigo.onlinelibraryapi.repositories.AuthorRepository;
 import jakarta.persistence.EntityNotFoundException;
@@ -13,9 +19,24 @@ import jakarta.persistence.EntityNotFoundException;
 @Service
 public class AuthorService {
 
-
     @Autowired
     private AuthorRepository authorRepository;
+
+    public Page<ListAuthorDTO> index(Pageable pageable, String name, Date dateBirth) {
+        Author author = new Author();
+        author.setName(name);
+        author.setDateBirth(dateBirth);
+        
+        var matcher = ExampleMatcher.matching()
+                .withIgnoreNullValues()
+                .withIgnoreCase()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+
+        Example<Author> example = Example.of(author, matcher);
+
+        Page<Author> authors = authorRepository.findAll(example, pageable);
+        return authors.map(ListAuthorDTO::new); 
+    }
 
     public Author create(CreateAuthorDTO data) {
 
@@ -36,11 +57,11 @@ public class AuthorService {
         return authorRepository.findByName(name)
                 .orElseThrow(() -> new EntityNotFoundException("Author with name " + name + " not found."));
     }
-    
 
     public boolean existsById(UUID id) {
         return authorRepository.existsById(id);
     }
+
     public boolean existsByName(String name) {
         return authorRepository.findByName(name).isPresent();
     }
