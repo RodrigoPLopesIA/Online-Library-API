e4474602ac19adc49b3c1b1a8471fe9f7f9cf53a
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-07-02 19:08:48 -0300

    fix: refactor SecurityFilterChain configuration for improved clarity and security settings

--- src/main/java/br/com/rodrigo/onlinelibraryapi/config/SpringSercurityConfig.java
+++ src/main/java/br/com/rodrigo/onlinelibraryapi/config/SpringSercurityConfig.java
@@ -4,9 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,25 +17,20 @@ public class SpringSercurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.csrf(csrf -> csrf.disable())
-                .httpBasic(basic -> basic.disable())
-                .formLogin(form -> form.disable())
-                /* .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.POST, "/api/v1/*")
-                
-                .permitAll()
-                        .anyRequest().authenticated())*/
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build();
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .authorizeRequests(authorize -> authorize.anyRequest().authenticated()).build();
     }
 
-
-    //@Bean
-    public PasswordEncoder passwordEncoder(){
+    // @Bean
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-
-    //@Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+    // @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
