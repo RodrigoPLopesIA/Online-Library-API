32b1b9cb8f614050c90f320158d853c2226eecda
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-07-02 23:29:42 -0300

    fix: add in-memory user details service with default users in SpringSercurityConfig

--- src/main/java/br/com/rodrigo/onlinelibraryapi/config/SpringSercurityConfig.java
+++ src/main/java/br/com/rodrigo/onlinelibraryapi/config/SpringSercurityConfig.java
@@ -6,12 +6,19 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import jakarta.persistence.criteria.CriteriaBuilder.In;
 
 @Configuration
 @EnableWebSecurity
@@ -25,9 +32,27 @@ public class SpringSercurityConfig {
                 .authorizeRequests(authorize -> authorize.anyRequest().authenticated()).build();
     }
 
-    // @Bean
+    @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+
+        UserDetails user = User
+                .builder()
+                .username("user")
+                .password(encoder.encode("password"))
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(encoder.encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
     // @Bean
