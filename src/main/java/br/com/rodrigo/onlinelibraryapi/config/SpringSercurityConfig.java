d5acbbf5e5b1f7db0f7ceec5b01b7a46d286d235
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-07-02 22:29:38 -0300

    fix: enable web security in SpringSercurityConfig and remove unused @EnableWebMvc annotation from WebConfiguration

--- src/main/java/br/com/rodrigo/onlinelibraryapi/config/SpringSercurityConfig.java
+++ src/main/java/br/com/rodrigo/onlinelibraryapi/config/SpringSercurityConfig.java
@@ -7,12 +7,14 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SpringSercurityConfig {
 
     @Bean
