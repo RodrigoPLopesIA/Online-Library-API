028cd1fccd11c25f01a0c212a02c368ae627049b
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-07-05 18:45:17 -0300

    docs: crreate book controller documentation

--- src/main/java/br/com/rodrigo/onlinelibraryapi/controllers/BookController.java
+++ src/main/java/br/com/rodrigo/onlinelibraryapi/controllers/BookController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import br.com.rodrigo.onlinelibraryapi.dtos.books.ListBookDTO;
 import br.com.rodrigo.onlinelibraryapi.dtos.user.ListUserDto;
 import br.com.rodrigo.onlinelibraryapi.entities.Book;
 import br.com.rodrigo.onlinelibraryapi.enums.Genre;
+import br.com.rodrigo.onlinelibraryapi.exceptions.UniqueViolationException;
 import br.com.rodrigo.onlinelibraryapi.mapper.BookMapper;
 import br.com.rodrigo.onlinelibraryapi.services.BookService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -33,9 +35,11 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import org.springframework.web.bind.annotation.RequestBody;
+
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 
-@Tag(name = "Books", description = "managing user-related operations in the Online Library API. Provides endpoints to create, retrieve, update, and delete books.")
+@Tag(name = "Books", description = "managing books-related operations in the Online Library API. Provides endpoints to create, retrieve, update, and delete books.")
 @RestController
 @RequestMapping("api/v1/books")
 public class BookController {
@@ -47,7 +51,7 @@ public class BookController {
     private BookMapper mapper;
 
     @Operation(summary = "Get all Books", responses = {
-            @ApiResponse(responseCode = "200", description = "Successfully retrieved list of books", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = ListUserDto.class))))
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved list of books", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = ListBookDTO.class))))
     })
     @GetMapping
     public ResponseEntity<Page<ListBookDTO>> index(@RequestParam(value = "title", required = false) String title,
@@ -61,11 +65,24 @@ public class BookController {
         return ResponseEntity.ok().body(books);
     }
 
+    @Operation(summary = "Retrieves a book by id", responses = {
+            @ApiResponse(responseCode = "200", description = "Retrieves a user successfully.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ListBookDTO.class))),
+            @ApiResponse(responseCode = "404", description = "book not found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = EntityNotFoundException.class))),
+    })
     @GetMapping("/{id}")
     public ResponseEntity<ListBookDTO> show(@PathVariable String id) {
         return ResponseEntity.ok().body(mapper.toDto(bookService.show(UUID.fromString(id))));
     }
 
+    @Operation(summary = "Create a book", responses = {
+            @ApiResponse(responseCode = "201", description = "Create book successfully.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ListBookDTO.class))),
+            @ApiResponse(responseCode = "409", description = "book already exists", content = @Content(mediaType = "application/json", schema = @Schema(implementation = UniqueViolationException.class))),
+            @ApiResponse(responseCode = "422", description = "Resource not processed", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MethodArgumentNotValidException.class))),
+            @ApiResponse(responseCode = "404", description = "Author not found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = EntityNotFoundException.class))),
+            @ApiResponse(responseCode = "400", description = "Book with ISBN already exists.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = IllegalArgumentException.class))),
+            @ApiResponse(responseCode = "400", description = "Book with title  already exists.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = IllegalArgumentException.class)))
+
+    })
     @PostMapping
     public ResponseEntity<ListBookDTO> create(@Valid @RequestBody CreateBookDTO data, UriComponentsBuilder uri) {
         Book book = bookService.create(mapper.toEntity(data));
@@ -75,12 +92,25 @@ public class BookController {
         return ResponseEntity.created(uriBuilder).body(mapper.toDto(book));
     }
 
+    @Operation(summary = "Update a book", responses = {
+            @ApiResponse(responseCode = "200", description = "Update book successfully.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ListBookDTO.class))),
+            @ApiResponse(responseCode = "409", description = "book already exists", content = @Content(mediaType = "application/json", schema = @Schema(implementation = UniqueViolationException.class))),
+            @ApiResponse(responseCode = "422", description = "Resource not processed", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MethodArgumentNotValidException.class))),
+            @ApiResponse(responseCode = "404", description = "Author not found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = EntityNotFoundException.class))),
+            @ApiResponse(responseCode = "400", description = "Book with ISBN already exists.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = IllegalArgumentException.class))),
+            @ApiResponse(responseCode = "400", description = "Book with title  already exists.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = IllegalArgumentException.class)))
+
+    })
     @PutMapping("/{id}")
     public ResponseEntity<ListBookDTO> update(@PathVariable String id, @Valid @RequestBody CreateBookDTO data) {
         Book book = bookService.update(UUID.fromString(id), mapper.toEntity(data));
         return ResponseEntity.ok().body(mapper.toDto(book));
     }
 
+    @Operation(summary = "delete book by id", responses = {
+            @ApiResponse(responseCode = "204", description = "delete a book successfully.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ListBookDTO.class))),
+            @ApiResponse(responseCode = "404", description = "book not found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = EntityNotFoundException.class))),
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<ListBookDTO> delete(@PathVariable String id) {
         bookService.delete(UUID.fromString(id));
