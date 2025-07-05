6f672729d92e8956c0e411a59954260f414aefe6
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-07-05 00:14:59 -0300

    feat: update authentication endpoint to allow public access for POST requests and adjust request mapping

--- src/main/java/br/com/rodrigo/onlinelibraryapi/config/SpringSercurityConfig.java
+++ src/main/java/br/com/rodrigo/onlinelibraryapi/config/SpringSercurityConfig.java
@@ -2,6 +2,7 @@ package br.com.rodrigo.onlinelibraryapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -24,7 +25,11 @@ public class SpringSercurityConfig {
                 .formLogin(form -> form.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(request -> {
-                    request.anyRequest().authenticated();
+                    request.requestMatchers(HttpMethod.POST, "/api/v1/auth")
+                            .permitAll()
+                            .anyRequest()
+                            .authenticated();
+
                 });
 
         return http.build();
--- src/main/java/br/com/rodrigo/onlinelibraryapi/controllers/AuthenticationController.java
+++ src/main/java/br/com/rodrigo/onlinelibraryapi/controllers/AuthenticationController.java
@@ -14,7 +14,7 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/v1/auth")
 public class AuthenticationController {
 
 
