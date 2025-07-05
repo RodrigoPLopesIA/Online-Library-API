eefd15a5a1c78dfd53cf461a8fcfc8f90268084c
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-07-05 00:15:36 -0300

    fix: correct import statement for RequestBody annotation in AuthenticationController

--- src/main/java/br/com/rodrigo/onlinelibraryapi/controllers/AuthenticationController.java
+++ src/main/java/br/com/rodrigo/onlinelibraryapi/controllers/AuthenticationController.java
@@ -6,11 +6,11 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.rodrigo.onlinelibraryapi.dtos.authentication.CredentialsDTO;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
 
 @RestController
